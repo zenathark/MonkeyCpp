cc4c0169bad78c213eeb54e3282a15b74fa7b7ee
zenathark <jcgalanh@gmail.com>
2023-02-04 07:40:49 -0800

    Add test suite for every single token

--- lexer_test.cc
+++ lexer_test.cc
@@ -2,16 +2,40 @@
 
 #include <memory>
 #include <sstream>
+#include <utility>
 
 #include "lexer.h"
+#include "token.h"
 
-TEST(LexerTest, WhenStreamHasSingleToken_ShouldReturnTokenType) {
-  auto expected_literal = "{";
-  auto expected_token_type = LEFTBRACE;
-  Lexer lexer{std::make_unique<std::istringstream>(expected_literal)};
+class LexerTest : public testing::TestWithParam<Token> {};
+
+TEST_P(LexerTest, WhenStreamHasSingleToken_ShouldReturnTokenType) {
+  auto expected_token = GetParam();
+  Lexer lexer{std::make_unique<std::istringstream>(expected_token.literal)};
 
   auto got = lexer.next();
 
-  EXPECT_EQ(got.token_type, expected_token_type);
-  EXPECT_EQ(got.literal, expected_literal);
+  EXPECT_EQ(got.token_type, expected_token.token_type);
+  EXPECT_EQ(got.literal, expected_token.literal);
 }
+
+Token createFromTokenType(std::string_view token_type) {
+  return Token{
+      .token_type = token_type,
+      .literal = std::string(token_type),
+  };
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    LexerTestParamInstantiation, LexerTest,
+    testing::Values(
+        createFromTokenType(ASTERISK), createFromTokenType(ASSIGN),
+        createFromTokenType(PLUS), createFromTokenType(MINUS),
+        createFromTokenType(BANG), createFromTokenType(SLASH),
+        createFromTokenType(EQUAL), createFromTokenType(NOTEQUAL),
+        createFromTokenType(LESSTHAN), createFromTokenType(GREATERTHAN),
+        createFromTokenType(COMMA), createFromTokenType(SEMICOLON),
+        createFromTokenType(COLON), createFromTokenType(LEFTPAREN),
+        createFromTokenType(RIGHTPAREN), createFromTokenType(LEFTBRACE),
+        createFromTokenType(RIGHTBRACE), createFromTokenType(LEFTBRACKET),
+        createFromTokenType(RIGHTBRACKET)));
---
+++ tests.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+build/lexer_test
